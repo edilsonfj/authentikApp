e9e60f0f7e41722ca2b82d755c6e07fffa4b6f5f
edilsonfj <edilson.desenvolved@gmail.com>
2023-08-11 17:30:20 -0300

    Criação de projeto Front-End utilizando React por meio do Vite, Typescript e Tailwind css. Configurado aquivo vite.confg, tailwind.config para adequação do projeto e importado fonts e imagens do projeto.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Ubuntu', 'sans-serif'],
+        'title': ['Oceanwide', 'sans-serif'],
+        'logo': ['Bebas Neue', 'sans-serif'],
+      },
+      screens: {
+        'screen': { 'max': '1920px' },
+        'desktop': { 'max': '1279px' },
+        'laptop': { 'max': '1023px' },
+        'tablet': { 'max': '767px' },
+        'mobile': { 'max': '429px' },
+      },
+      backgroundImage: {
+        'astroOne': "url('./src/assets/astroOne.png')",
+        'astroTwo': "url('./src/assets/astroTwo.png')",
+        'rocket': "url('./src/assets/rocket.png')",
+        'rocketRed': "url('./src/assets/rocketRed.png')",
+        'nick': "url('./src/assets/nick.png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
